8d7617e8c0beb61c0ac15c04bced62a5023b24e0
swetapatel27 <sweta.patel@rku.ac.in>
2020-08-10 12:58:57 +0530

    editing

--- connect.py
+++ connect.py
@@ -2,17 +2,16 @@ import sqlite3
 class myconnect:
       
       def __init__(self):
-            self.conn = sqlite3.connect("emp.db")
-            
+              
             try:
-                  self.conn.execute('create table if not exists emp (name text, salary interger, emp_type char)')
-                  print('table created')
+                self.conn = sqlite3.connect("emp.db")
+                self.conn.execute('create table if not exists emp (name text, salary interger, emp_type char)')
+                print('table created')
             except:
-                  pass
+                pass
                  
                   
       def savetodb(self,ename,eemail,emob,etype,eexp,esalary):
-            #6
-
-      # def display(self):
-      #       #7
+          pass
+      def display(self):
+          pass
